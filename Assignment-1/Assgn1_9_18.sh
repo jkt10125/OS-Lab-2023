adb47f71bd4e2cecb72bb92160bc991e28acdd18
abhay-k47 <99312837+abhay-k47@users.noreply.github.com>
2023-01-21 13:20:11 +0530

    Add files via upload

---
+++ Assignment-1/Assgn1_9_18.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+cat $1|cut -d" " -f2|sort|uniq -c|sort -k 1nr -k2
+# cat majors.txt | awk  '{arr[$2]++; } END {for(dep in arr) printf "%s %d\n", dep , arr[dep]}' | sort  -k 2nr -k1
+cat $1|cut -d" " -f1|sort|uniq -c|awk '$1 > 1 {print $2}'
+cat $1|cut -d" " -f1|sort|uniq -c|awk '$1 == 1 {count++} END {print count}'
+
