14b22b836059b6533a88f568e1728b0faf7f6bde
Aniket Kumar <ani30r.ak@gmail.com>
2023-01-22 04:51:47 +0530

    updated Q9

--- Assignment-1/Assgn1_9_18.sh
+++ Assignment-1/Assgn1_9_18.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-
 cat $1|cut -d" " -f2|sort|uniq -c|sort -k 1nr -k2
 # cat majors.txt | awk  '{arr[$2]++; } END {for(dep in arr) printf "%s %d\n", dep , arr[dep]}' | sort  -k 2nr -k1
-cat $1|cut -d" " -f1|sort|uniq -c|awk '$1 > 1 {print $2}'
-cat $1|cut -d" " -f1|sort|uniq -c|awk '$1 == 1 {count++} END {print count}'
+cat $1|cut -d" " -f1|sort|uniq -c|tee >(awk '$1 > 1 {print $2}') >(awk '$1 == 1 {count++} END {print count}') >/dev/null
+# cat $1|cut -d" " -f1|sort|uniq -c|awk '$1 == 1 {count++} END {print count}'
 
