02d18f1077330b93be23eb259a26af374f09905a
Aniket Kumar <ani30r.ak@gmail.com>
2023-01-22 04:03:24 +0530

    Q1 fully done and Giving correct Output

--- Assignment-1/Assgn1_1_18.sh
+++ Assignment-1/Assgn1_1_18.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-gcd() {
-    [ $(($1%$2)) -eq 0 ] && return $2 || gcd $2 $(($1%$2))
-}
+gcd() { 
+    [ $(($1%$2)) == 0 ]&&echo $2||gcd $2 $(($1%$2))
+ }
 lcm=1
-arr=(`rev $1 | sed 's/^0*//'`)
-for p in ${arr[@]}
+arr=(`rev $1|sed 's/^0*//'`)
+for p in ${arr[@]} 
 do
-    gcd $lcm $p
-    lcm=$((lcm*p/$?))
+    t=$(gcd $lcm $p)
+    lcm=$((p/t*lcm))
 done
 echo $lcm
