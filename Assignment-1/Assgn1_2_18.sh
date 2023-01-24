18c04050a23bb8176a7d0253bee3765767d00e73
jkt10125 <jkt10125@gmail.com>
2023-01-25 04:20:25 +0530

    improved word count

--- Assignment-1/Assgn1_2_18.sh
+++ Assignment-1/Assgn1_2_18.sh
@@ -2,7 +2,7 @@
 while read username
 do
     output="NO"
-    if grep -Pq "^[a-zA-Z](?=.*[0-9])[a-zA-Z0-9]{4,19}$" <<< "$username" && ! grep -iqFf fruits.txt <<< "$username"
+    if grep -Pq "^[a-zA-Z](?=.*[0-9])[a-zA-Z0-9]{4,19}$" <<< "$username"&&! grep -iqFf fruits.txt <<< "$username"
     then
     output="YES"
     fi
