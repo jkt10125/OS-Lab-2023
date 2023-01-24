2649cd1bb8e492c53d30eb4834330f6526836b00
Jay Kumar Thakur <75112567+jkt10125@users.noreply.github.com>
2023-01-25 00:45:12 +0530

    Update Assgn1_1_18.sh

--- Assignment-1/Assgn1_1_18.sh
+++ Assignment-1/Assgn1_1_18.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 gcd() { 
-    [ $(($1%$2)) == 0 ]&&echo $2||gcd $2 $(($1%$2))
- }
+    [ $(($1%$2)) -eq 0 ]&&echo $2||gcd $2 $(($1%$2))
+}
 lcm=1
-for p in `rev $1|sed 's/^0*//'`
+for p in `rev $1 | sed 's/^0*//'`
 do
     lcm=$((p/$(gcd $lcm $p)*lcm))
 done
