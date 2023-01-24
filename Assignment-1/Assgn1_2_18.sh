31009a65409e402b92cb9e2f729e927d030a0c70
Aniket Kumar <ani30r.ak@gmail.com>
2023-01-24 18:17:38 +0530

    pushhh

--- Assignment-1/Assgn1_2_18.sh
+++ Assignment-1/Assgn1_2_18.sh
@@ -5,13 +5,6 @@ do
     if grep -Pq "^[a-zA-Z](?=.*[0-9])[a-zA-Z0-9]{4,19}$" <<< "$username" && ! grep -iqFf fruits.txt <<< "$username"
     then
     output="YES"
-    # while read fruits
-    # do
-    #     if grep -i -q "$fruits"<<<"$username" 
-    #     then
-    #         output="NO"
-    #     fi
-    # done<fruits.txt
     fi
     echo "$output">>validation_results.txt
 done<$1
