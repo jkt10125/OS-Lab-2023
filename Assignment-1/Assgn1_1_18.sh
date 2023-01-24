a54018f6af0f15ca1f6e38403e8cada510f9013e
jkt10125 <jkt10125@gmail.com>
2023-01-25 03:35:16 +0530

    improved word count

--- Assignment-1/Assgn1_1_18.sh
+++ Assignment-1/Assgn1_1_18.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
-gcd() { 
-    [ $(($1%$2)) -eq 0 ]&&echo $2||gcd $2 $(($1%$2))
+#!/bin/bash
+function gcd {
+    test `expr $1 % $2` -eq 0&&echo $2||gcd $2 `expr $1 % $2`    
 }
 lcm=1
 for p in `rev $1 | sed 's/^0*//'`
 do
-    lcm=$((p/$(gcd $lcm $p)*lcm))
+    lcm=`expr $p / \`gcd $lcm $p\` \* $lcm`
 done
 echo $lcm
