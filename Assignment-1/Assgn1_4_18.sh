c09947a4316a272604a1af7f0e2e2cc747311acd
abhay-k47 <99312837+abhay-k47@users.noreply.github.com>
2023-01-24 15:48:33 +0530

    Update Assgn1_4_18.sh

--- Assignment-1/Assgn1_4_18.sh
+++ Assignment-1/Assgn1_4_18.sh
@@ -4,7 +4,7 @@ IFS=$'\n'
 while read line
 do
     [[ $line =~ .*$2.* ]] &&
-    echo $line | sed 's/\([a-zA-Z]\)\([a-zA-Z]\)/\U\1\L\2/g' >>temp.txt ||
+    echo $line | sed -e 's/\([a-zA-Z]\)\([^a-zA-Z]*\)\([a-zA-Z]\)/\U\1\E\2\L\3\E/g' >>temp.txt ||
     echo $line >>temp.txt
 done <$1
 mv temp.txt input1.txt
