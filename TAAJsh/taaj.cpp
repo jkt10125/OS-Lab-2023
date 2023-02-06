7b95ffe01c36874960e579cab48be0338755963e
Jay Kumar Thakur <75112567+jkt10125@users.noreply.github.com>
2023-02-06 14:17:35 +0530

    Added bug fixes

--- TAAJsh/taaj.cpp
+++ TAAJsh/taaj.cpp
@@ -1,23 +1,20 @@
 #include <unistd.h>
 #include <termios.h>
-// #include <stdio.h>
-#include <string>
-#include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <iostream>
+#include <string.h>
 #include <sys/wait.h>
+
 using namespace std;
-const int MAX_LINE = 4096;
+
 struct termios orig_termios;
 
-void disableRawMode()
-{
+void disableRawMode() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-void enableRawMode()
-{
+void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
 
@@ -28,67 +25,52 @@ void enableRawMode()
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-void die(const string &s)
-{
+void die(const string s) {
     perror(s.c_str());
-    exit(1);
+    exit(0);
 }
 
-int main()
-{
+const int MAXCHAR = 100;
+const int MAXARGS = 100;
+
+int main() {
     enableRawMode();
 
-    char *args[MAX_LINE / 2 + 1]; /* Command line arguments */
-    char input[MAX_LINE];         /* User input */
-    int should_run = 1;           /* Flag to determine when to exit program */
-
-    while (should_run)
-    {
-        char username[256], hostname[256], curr_dir[256];
-        getlogin_r(username, 256);
-        gethostname(hostname, 256);
-        getcwd(curr_dir, 256);
-        cout << "\033[1;32m" << username << "@" << hostname << "\033[0m"
-             << ":"
-             << "\033[1;31m" << curr_dir << "\033[0m"
-             << "$";
+    while (true) {
+        char uname[256], hname[256], cdir[256];
+        getlogin_r(uname, 256);
+        gethostname(hname, 256);
+        getcwd(cdir, 256);
+        
+        cout << "\033[1;32m" << uname << "@" << hname << ":" << "\033[1;33m" << cdir << "\033[0m" << "$ ";
         fflush(stdout);
-        // cout << u8"\u276f" << endl;
-        fgets(input, MAX_LINE, stdin);
-        // cin.getline(input, MAX_LINE, '\r');
-        getchar();
-        cout<<input<<endl;
-        input[strcspn(input, "\n")] = 0;
 
-        int i = 0;
+        char input[MAXCHAR];
+        char *args[MAXARGS];
+
+        fgets(input, MAXCHAR, stdin);
+
         char *token = strtok(input, " \t\n");
-        while (token != NULL)
-        {
-            args[i++] = token;
+        int ctr = 0;
+        while (token != NULL) {
+            args[ctr++] = token;
             token = strtok(NULL, " ");
         }
-        args[i] = NULL;
+        args[ctr] = NULL;
 
         pid_t pid = fork();
-        if (pid == 0)
-        {
-            /* Child process */
+        if (pid == 0) {
             execvp(args[0], args);
-            perror("Error executing command");
-            exit(1);
+            exit(0);
         }
-        else if (pid < 0)
-        {
-            /* Error forking */
-            perror("Error forking");
-            exit(1);
+        else if (pid < 0) {
+            die("Error in forking!");
         }
-        else
-        {
-            /* Parent process */
+        else {
             int status;
             waitpid(pid, &status, 0);
         }
     }
+
     return 0;
 }
