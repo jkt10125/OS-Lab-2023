f2735f1ecb5386c9d0d0b5a25d1f8309c5e7c120
abhay-k47 <99312837+abhay-k47@users.noreply.github.com>
2023-02-06 22:18:14 +0530

    Create utils.hpp

---
+++ TAAJsh/utils.hpp
@@ -0,0 +1,42 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+#include <iostream>
+#include <vector>
+#define CTRL_KEY(k) ((k)&0x1f)
+
+enum editorKey
+{
+    TABLINE = 9,
+    NEWLINE = 13,
+    BACKSPACE = 127,
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
+
+// Trims leading and trailing whitespaces of a string
+void trim(std::string &str);
+// Splits an input string on the basis of a delimiter
+std::vector<std::string> split(std::string &str, char delim);
+
+// Sends one line in the form of a string
+std::string ReadLine();
+
+int editorReadKey();
+
+// Ctrl key press handler
+void processCtrl(char c);
+
+void die(const std::string &s);
+
+void disableRawMode();
+
+void enableRawMode();
+
+void shellPrompt();
+
+//Function to execute commands 
+void execute_command(std::vector<std::string> command);
+
+#endif
