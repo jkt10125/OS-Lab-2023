23962b3b6a7a9c1301dc7ae181a26e4f17c8058f
jkt10125 <jkt10125@gmail.com>
2023-03-20 19:56:31 +0530

    added guest kicking idea

--- Assignment-5/definitions.h
+++ Assignment-5/definitions.h
@@ -21,9 +21,11 @@ public:
     int Priority;
 };
 
+pthread_mutex_t kickedGuestMutex = PTHREAD_MUTEX_INITIALIZER;
+
 extern std::vector<Guest> Guests;
 extern std::vector<Room> Rooms;
-extern int RoomSize;
+extern int RoomSize, kickedGuest;
 extern std::vector<sem_t> roomSemaphore;
 
 void initGuests(int Y);
--- Assignment-5/guest.cpp
+++ Assignment-5/guest.cpp
@@ -1,4 +1,5 @@
 #include "definitions.h"
+#include <ctime>
 
 using namespace std;
 
@@ -19,32 +20,61 @@ void *simulateGuests(void *params)
     while (true)
     {
         sleep(rand() % 11 + 10);
-        int stayTime = (rand() % 21 + 10);
+        int stayTime = rand() % 21 + 10;
+        time_t exitTime = time(nullptr) + stayTime;
 
         // acquire room part
         int i = 0;
+        bool ok = false;
         for (i; i < RoomSize; i++)
         {
             sem_wait(&roomSemaphore[i]);
             if (Rooms[i].currentGuest == -1 && (Rooms[i].firstGuestTime == -1 || Rooms[i].secondGuestTime == -1))
             {
                 Rooms[i].currentGuest = ID;
+                ok = true;
             }
             sem_post(&roomSemaphore[i]);
         }
-        sleep(stayTime);
 
-        sem_wait(&roomSemaphore[i]);
-        if (Rooms[i].currentGuest == ID){
-            Rooms[i].currentGuest = -1;
-            if(Rooms[i].firstGuestTime == -1){
+        // if the guest gets a room then he stays until stay_timer expires or he is kicked out
+        if (ok)
+        {
+            time_t currentTime;
+
+            while (ID != kickedGuest && (currentTime = time(nullptr)) < exitTime) ;
+
+            // if he is kicked out then he updates his kickedGuest variable
+            if (ID == kickedGuest)
+            {
+                pthread_mutex_lock(&kickedGuestMutex);
+                kickedGuest = -1;
+                pthread_mutex_unlock(&kickedGuestMutex);
+            }
+
+            stayTime -= (exitTime > currentTime) ? (exitTime - currentTime) : 0;
+            
+            // updating the room info
+            sem_wait(&roomSemaphore[i]);
+            if (Rooms[i].firstGuestTime == -1)
+            {
                 Rooms[i].firstGuestTime = stayTime;
             }
-            else{
+            else
+            {
                 Rooms[i].secondGuestTime = stayTime;
             }
+            sem_post(&roomSemaphore[i]);
         }
-        sem_post(&roomSemaphore[i]);
+
+        // else he seeks a lower priority guest occupying a room and kicks him
+        
+        else {
+
+            
+
+        }
+
         // End acquire room part
     }
 }
